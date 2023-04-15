29255c0307303d22380f84603f6c22a08853a24c
Crowdbotics <team@crowdbotics.com>
2023-04-15 22:36:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-fog-39959",
+  url: "https://fragrant-fog-39959.botics.co",
+  api: "https://fragrant-fog-39959.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
